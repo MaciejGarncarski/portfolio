fc00c7cf0442c36a014981cec953f415162b288f
Maciej Garncarski <maciejgarncarski@protonmail.com>
2023-05-20 11:05:13 +0200

    chore: update astro package

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://maciej-garncarski.pl',
+  compressHTML: true,
   integrations: [
     tailwind({
       config: {
