13696df3f6e8be5b398e826aa839168492833bfc
Maciej Garncarski <maciejgarncarski@protonmail.com>
2023-06-01 08:12:16 +0200

    chore: small improvements

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Portfolio
 
+My personal website/portfolio. Created in Astro framework. I've choosen Astro because, it's really fast and simple to learn. I used new view transition API - it allows to create transition in MPA's. In Chrome browser you have to enable `viewTransition API for navigations` flag in `chrome://flags/` to see the transitions.
+
+## Performance
+
+![Lighthouse performance](https://raw.githubusercontent.com/MaciejGarncarski/portfolio/master/.github/lighthouse.png)
+
 ## Live
 
 https://maciej-garncarski.pl/
--- src/i18n/translations/home.ts
+++ src/i18n/translations/home.ts
@@ -3,12 +3,12 @@ export const home = {
     'home.tech.heading': 'My tech stack',
     'home.description':
       'Enthusiast of creating applications, websites and other cool things related to the internet. I will create professionall and lightning-fast website for you!',
-    'home.link-contact': 'Contact me'
+    'home.link-contact': 'Contact'
   },
   pl: {
     'home.description':
       'Entuzjasta tworzenia aplikacji, stron i wielu innych fajnych rzeczy związanych z internetem. Stworzę dla Ciebie profesjonalną i błyskawicznie szybką stronę internetową!',
     'home.tech.heading': 'Znam te technologie',
-    'home.link-contact': 'Zadaj pytanie'
+    'home.link-contact': 'Kontakt'
   }
 };
