da07ae6a3f987c0a1fbb7cfb573dba0a4fb4c412
Maciej Garncarski <maciejgarncarski@protonmail.com>
2024-02-06 21:58:06 +0100

    feat: add copy buttons

--- src/i18n/translations/contact.ts
+++ src/i18n/translations/contact.ts
@@ -1,10 +1,12 @@
 export const contact = {
   en: {
     'contact.heading': 'Contact',
-    'contact.phone-number': 'Phone number'
+    'contact.phone-number': 'Phone number',
+    'contact.copy-text': 'Copy text'
   },
   pl: {
     'contact.heading': 'Kontakt',
-    'contact.phone-number': 'Numer telefonu'
+    'contact.phone-number': 'Numer telefonu',
+    'contact.copy-text': 'Kopiuj tekst'
   }
 };
