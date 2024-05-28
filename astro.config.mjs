57c9c568accd57de38e7e976dd480b554e703d57
Maciej Garncarski <maciejgarncarski@protonmail.com>
2024-05-28 21:39:06 +0200

    feat: prefetching links, improve transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: true,
   site: 'https://maciej-garncarski.pl',
   compressHTML: true,
   build: {
