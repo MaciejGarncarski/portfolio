5aebe896aaf63037b2a4f695c85871d664cc21fc
Maciej Garncarski <maciejgarncarski@protonmail.com>
2024-07-22 22:34:55 +0200

    chore: remove unused font weight

--- src/utils/seo.ts
+++ src/utils/seo.ts
@@ -1,6 +1,6 @@
 export const title = 'Maciej Garncarski';
 export const keywords =
-  'junior, junior-front-end, junior front end, frontend, frontend developer, front-end, junior javascript, javascript, typescript, TypeScript, JavaScript, React, Next.js, Astro, HTML, CSS, strony internetowe, strony internetowe Dębica, aplikacje internetowe Dębica, Podgrodzie, Maciej Garncarski, Garncarski, Maciej, Maga górna, wizytówka, wizytówka internetowa, internet, Sass, Jest, websites, webapps, PWA, Progressive web app, Progressive web apps, Pilzno, Tanie strony internetowe, Szybkie strony internetowe';
+  'junior, junior-front-end, junior front end, frontend, frontend developer, front-end, junior javascript, javascript, typescript, TypeScript, JavaScript, React, Next.js, Astro, HTML, CSS, strony internetowe, strony internetowe Dębica, aplikacje internetowe Dębica, Podgrodzie, Maciej Garncarski, Garncarski, Maciej, Maga górna, wizytówka, wizytówka internetowa, internet, Sass, PostCSS, vitest, Jest, websites, webapps, PWA, Progressive web app, Progressive web apps, Pilzno, Tanie strony internetowe, Szybkie strony internetowe';
 export const description = {
   pl: 'Entuzjasta tworzenia aplikacji, stron i wielu innych fajnych rzeczy związanych z internetem.',
   en: 'Enthusiast of creating applications, websites and other cool things related to the internet.'
