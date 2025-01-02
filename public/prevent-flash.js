c2ed36be37c3a556f59ee07bd39b581a14ac0fdd
Maciej Garncarski <maciejgarncarski@protonmail.com>
2025-01-02 12:03:06 +0100

    fix: script

--- public/prevent-flash.js
+++ public/prevent-flash.js
@@ -1,6 +1,6 @@
 const theme = localStorage.getItem("theme");
 
-if (!theme) {
+if (theme === "system") {
   if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
     document.documentElement.classList.add("dark");
   } else {
