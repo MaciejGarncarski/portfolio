b4caf7f54b2df8227af8edd986469cdfae030893
Maciej Garncarski <maciejgarncarski@protonmail.com>
2025-02-04 19:52:43 +0100

    feat: custom keywords

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,6 +5,7 @@ const blog = defineCollection({
   loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
   schema: z.object({
     title: z.string(),
+    keywords: z.array(z.string()),
     description: z.string(),
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
