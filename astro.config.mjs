9964fe1df6be21d891aa9921033261e546021908
Maciej Garncarski <maciejgarncarski@protonmail.com>
2025-02-06 22:17:52 +0100

    fix: typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig, envField } from "astro/config";
+import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 
@@ -8,20 +8,6 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: "https://maciej-garncarski.pl",
-  env: {
-    schema: {
-      SPOTIFY_CLIENT_ID: envField.string({
-        min: 10,
-        context: "server",
-        access: "secret"
-      }),
-      SPOTIFY_SECRET: envField.string({
-        min: 10,
-        context: "server",
-        access: "secret"
-      })
-    }
-  },
   markdown: {
     shikiConfig: {
       themes: {
