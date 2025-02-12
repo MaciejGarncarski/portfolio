2034ef55643e0eb5468dcf9de6fd9da00ec2506e
Maciej Garncarski <maciejgarncarski@protonmail.com>
2025-02-12 16:08:45 +0100

    feat: better code block

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,33 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import sitemap from "@astrojs/sitemap";
 
 import mdx from "@astrojs/mdx";
+import rehypePrettyCode from "rehype-pretty-code";
+
+const prettyCodeOptions = {
+  theme: {
+    light: "github-light",
+    dark: "github-dark"
+  },
+  onVisitHighlightedLine(node) {
+    node?.properties?.className?.push("highlighted");
+  },
+  onVisitHighlightedChars(node) {
+    console.log(node);
+    node?.properties?.className
+      ? node.properties.className.push("highlighted-chars")
+      : (node.properties.className = ["highlighted-chars"]);
+  },
+  tokensMap: {}
+};
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://maciej-garncarski.pl",
   integrations: [sitemap(), mdx()],
   markdown: {
-    shikiConfig: {
-      themes: {
-        light: "github-light",
-        dark: "github-dark"
-      }
-    }
+    syntaxHighlight: false,
+    rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]]
   }
 });
