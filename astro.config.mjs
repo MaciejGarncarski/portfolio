ace13c40664c0cdab207d2096f5fe9e8214aa9bd
Maciej Garncarski <maciejgarncarski@protonmail.com>
2025-02-13 22:05:57 +0100

    feat: better code blocks design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,11 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
 import mdx from "@astrojs/mdx";
 import rehypePrettyCode from "rehype-pretty-code";
-
-const prettyCodeOptions = {
-  theme: {
-    light: "github-light",
-    dark: "nord"
-  },
-  onVisitHighlightedLine(node) {
-    node?.properties?.className?.push("highlighted");
-  },
-  onVisitHighlightedChars(node) {
-    console.log(node);
-    node?.properties?.className
-      ? node.properties.className.push("highlighted-chars")
-      : (node.properties.className = ["highlighted-chars"]);
-  },
-  tokensMap: {}
-};
+import {
+  transformerNotationDiff,
+  transformerNotationFocus
+} from "@shikijs/transformers";
 
 // https://astro.build/config
 export default defineConfig({
@@ -28,6 +13,33 @@ export default defineConfig({
   integrations: [sitemap(), mdx()],
   markdown: {
     syntaxHighlight: false,
-    rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]]
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        {
+          transformers: [
+            transformerNotationDiff({
+              matchAlgorithm: "v3"
+            }),
+            transformerNotationFocus({
+              matchAlgorithm: "v3"
+            })
+          ],
+          keepBackground: false,
+          theme: {
+            light: "github-light",
+            dark: "nord"
+          },
+          onVisitHighlightedLine(node) {
+            node?.properties?.className?.push("highlighted");
+          },
+          onVisitHighlightedChars(node) {
+            node?.properties?.className
+              ? node.properties.className.push("highlighted-chars")
+              : (node.properties.className = ["highlighted-chars"]);
+          }
+        }
+      ]
+    ]
   }
 });
