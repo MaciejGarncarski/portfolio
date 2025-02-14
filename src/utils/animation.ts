ada3d161a9f0922f7c67d2fddef7b0d49466ad28
Maciej Garncarski <maciejgarncarski@protonmail.com>
2025-02-14 18:32:07 +0100

    chore: improve view transitions

---
+++ src/utils/animation.ts
@@ -0,0 +1,25 @@
+import type {
+  TransitionAnimation,
+  TransitionAnimationPair,
+  TransitionDirectionalAnimations
+} from "astro";
+
+export const mainEntranceAnimationIn: TransitionAnimation = {
+  name: "mainEntrance",
+  duration: "0.3s"
+};
+
+export const mainEntranceAnimationOut: TransitionAnimation = {
+  name: "mainEntranceOut",
+  duration: "0.15s"
+};
+
+export const mainEntrancePair: TransitionAnimationPair = {
+  old: mainEntranceAnimationOut,
+  new: mainEntranceAnimationIn
+};
+
+export const mainEntrance: TransitionDirectionalAnimations = {
+  forwards: mainEntrancePair,
+  backwards: mainEntrancePair
+};
