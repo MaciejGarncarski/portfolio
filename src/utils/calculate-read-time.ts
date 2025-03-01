4d9adab0218e8b131caa322005278746d55a1d9e
Maciej Garncarski <maciejgarncarski@protonmail.com>
2025-03-01 18:05:11 +0100

    feat: update WPM

--- src/utils/calculate-read-time.ts
+++ src/utils/calculate-read-time.ts
@@ -1,5 +1,5 @@
 export function calculateReadingTime(mdxContent: string) {
-  const wordsPerMinute = 200;
+  const wordsPerMinute = 185;
 
   const text = mdxContent.replace(/<\/?[^>]+(>|$)/g, "");
   const wordCount = text
